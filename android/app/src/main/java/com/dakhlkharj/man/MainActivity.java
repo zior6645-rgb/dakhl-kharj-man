6abe984fcfe45f2b6b21f7d1e7e3d21921a23a7e
zior6645-rgb <zior6645@gmail.com>
2026-09-23 02:06:40 +0330

    Register native Android file saver plugin

--- android/app/src/main/java/com/dakhlkharj/man/MainActivity.java
+++ android/app/src/main/java/com/dakhlkharj/man/MainActivity.java
@@ -2,4 +2,10 @@ package com.dakhlkharj.man;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        registerPlugin(FileSaverPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
